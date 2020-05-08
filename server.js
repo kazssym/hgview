076d12c9be0b7a65052785b58d6a22b9b86bc46f
Kaz Nishimura <kazssym@linuxfront.com>
2020-05-09 05:06:44 +0900

    Print the listening port

--- server.js
+++ server.js
@@ -45,8 +45,12 @@ function main(args)
     return new Promise(async (resolve, reject) =>
         {
             let app = express();
-            app.use("/", express.static(`${__dirname}/web`));
-            app.listen(port);
+            app.use("", express.static(`${__dirname}/web`));
+            app.listen(port, () =>
+                {
+                    console.log("%d", port);
+                }
+            );
         }
     );
 }

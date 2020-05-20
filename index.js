53cf7e047162913f30b0d5fe39a581332ea52297
Kaz Nishimura <kazssym@linuxfront.com>
2020-05-20 13:38:50 +0900

    Define a new class.

--- index.js
+++ index.js
@@ -18,4 +18,51 @@
 
 "use strict";
 
-// Nothing is defined yet.
+let express = require("express");
+
+const DEFAULT_PORT = 3000;
+
+/**
+ * Application class.
+ */
+class App
+{
+    constructor()
+    {
+        this._serverOptions = {
+            port: DEFAULT_PORT,
+        };
+        this._app = null;
+    }
+
+    get port()
+    {
+        return this._serverOptions.port;
+    }
+
+    set port(port)
+    {
+        this._serverOptions.port = port;
+    }
+
+    /**
+     * Runs a HTTP server.
+     */
+    async run()
+    {
+        this._app = express();
+        this._app.use("", express.static(`${__dirname}/web`));
+
+        await new Promise(
+            (_resolve, reject) => {
+                this._app.listen(this._serverOptions,
+                    () => {
+                    })
+                .on("error",
+                    (e) => {
+                        reject(e);
+                    });
+            });
+    }
+}
+module.exports.App = App;
